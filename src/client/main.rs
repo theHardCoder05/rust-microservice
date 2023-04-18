ba00d2d905adf8a95623ca96408188080be78dfd
HardCoder <bitlabs27@gmail.com>
2023-04-18 19:46:28 +0900

    halfway done implementation

--- src/client/main.rs
+++ src/client/main.rs
@@ -12,10 +12,12 @@ pub mod authentication {
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct cli {
-    #[command(Subcommand)]
+struct Cli {
+    #[command(subcommand)]
     command: Option<Commands>,
 }
+
+
 
 
 #[derive(Subcommand)] 
@@ -42,12 +44,46 @@ enum Commands {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn, std::error::Error>>{
+
+
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let auth_ip = env::var("AUTH_SERVICE_IP").unwrap_or("[::0]".to_owned());
     let mut client = AuthClient::connect(format!("http://{}:50051", auth_ip)).await?;
     let cli = Cli::parse();
 
     match &cli.command {
-        
+        Some(Commands::SignIn {username, password}) => {
+            let request = tonic::Request::new(SignInRequest{
+                username: username.clone(),
+                password: password.clone(),
+            });
+
+            let response = client.sign_in(request).await?.into_inner();
+            println!("{:?}", response);
+        }
+
+        Some(Commands::SignUp { username, password}) => {
+            let request = tonic::Request::new(SignUpRequest{
+                username: username.clone(),
+                password: password.clone(),
+            });
+
+            let response = client.sign_up(request).await?;
+            println!("{:?}", response.into_inner());
+        }
+
+
+        Some(Commands::SignOut {session_token}) => {
+            let request = tonic::Request::new(SignOutRequest{
+                session_token: session_token.clone(),
+            });
+
+            let response = client.sign_out(request).await?;
+
+            println!("{:?}", response.into_inner());
+        }
+        None => {}
     }
+
+    Ok(())
 }
